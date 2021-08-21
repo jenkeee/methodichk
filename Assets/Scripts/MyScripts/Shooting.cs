81340cd1b7eaa0b4c8cdfd66fd758b3d5ab11078
“[firstname <83080324+jenkeee@users.noreply.github.com>
2021-08-21 13:36:52 +0300

    подправил контролер и стрельбу
    завтра по плану повреждения. возможно добавлю мину, незабыть главное про прыжок.

--- Assets/Scripts/MyScripts/Shooting.cs
+++ Assets/Scripts/MyScripts/Shooting.cs
@@ -22,8 +22,9 @@ public class Shooting : MonoBehaviour
     public Vector3 target { get; set; }
     public bool hit { get; set; }
     [SerializeField]
-    private Transform bulletParent;
-    private Plane plane = new Plane(Vector3.up, Vector3.zero);
+    private Transform bulletPoolTr;
+
+    int currentBulletIndex = 0;
 
     private void Awake()
     {
@@ -36,20 +37,66 @@ public class Shooting : MonoBehaviour
     {
         
     }
+    private static void DisappearanceLogic(GameObject gameObject)
+    {
+        int num = 0;
+        while (num >= 0)
+        {
+            if (num % 2 == 0)
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+            }
+            num++;
+        }
+    }
 
-    // Update is called once per frame
-    void Update()
+// Update is called once per frame
+void Update()
     {
 
-     
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+             if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            int toDisable = currentBulletIndex - 5;
+            if (toDisable < 0) toDisable = 0;
+            Rigidbody bulletDisable = bulletPoolTr.GetChild(toDisable).GetComponent<Rigidbody>();
 
-           
+
+            Rigidbody bulletRB = bulletPoolTr.GetChild(currentBulletIndex).GetComponent<Rigidbody>();         
+            bulletRB.gameObject.SetActive(true);
+            bulletDisable.gameObject.SetActive(false);
+            bulletRB.position = ShootingPoint.position;
+            bulletRB.velocity = Vector3.zero;            
+            bulletRB.AddForce(Camera.main.transform.forward * bulletSpeed, ForceMode.Impulse);
+            currentBulletIndex++;
+            if (currentBulletIndex >= bulletPoolTr.childCount) {
+                currentBulletIndex = 0;                
+            }
+
+
+
+            //DisappearanceLogic(bulletRB.gameObject);
+
+            /*
+             * Rigidbody bulletRB = bulletPoolTr.GetChild(currentBulletIndex).GetComponent<Rigidbody>();
+            bulletRB.gameObject.SetActive(true);
+            bulletRB.position = transform.position + Vector3.right;
+            bulletRB.velocity = Vector3.zero;
+            Vector3 pushDir = bulletRB.transform.position - Camera.main.transform.position;
+            bulletRB.AddForce(pushDir.normalized * bulletSpeed, ForceMode.Impulse);
+            currentBulletIndex++;
+            if (currentBulletIndex >= bulletPoolTr.childCount) {
+                currentBulletIndex = 0;
+            }*/
+            /* тут я создавал и удалял щас делаю пулинг
             GameObject temp = Instantiate(Bullet,ShootingPoint.position, Quaternion.identity); // создаем пулю 
             Rigidbody tempRB = temp.GetComponent<Rigidbody>();
             Vector3 pushDir = temp.transform.position - Camera.main.transform.position;          
             tempRB.AddForce(pushDir.normalized * bulletSpeed, ForceMode.Impulse);
-            Destroy(temp, 5f);
+            Destroy(temp, 5f);*/
         }
     }   
 }
